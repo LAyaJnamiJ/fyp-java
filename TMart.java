0f452ef259530901cc719f9379591bb4cbdce63e
LAyaJnamiJ <layajnamij@gmail.com>
2022-05-05 13:20:45 +0800

    Added new files

---
+++ TMart.java
@@ -0,0 +1,225 @@
+import java.awt.image.BufferedImage;
+import java.util.*;
+
+import javax.imageio.ImageIO;
+
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+public class TMart
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		
+		   String csvFile = "1225.csv";
+	        BufferedReader br = null;
+	        String line = "";
+	        String cvsSplitBy = ",";
+
+	        BufferedReader brcount = new BufferedReader(new FileReader(csvFile));
+	        int count = 0;
+	        while(brcount.readLine() != null)
+	        {
+	            count++;
+	        }
+			brcount.close();
+	  
+	        double[][] DoubleMatrix= new double[count][count];
+	        try {
+	        	int row= 0; 
+	            br = new BufferedReader(new FileReader(csvFile));
+	            while ((line = br.readLine()) != null) {
+
+	                // use comma as separator
+	                String[] country = line.split(cvsSplitBy);
+
+	                for(int i=0;i<count;i++) DoubleMatrix[row][i]=Double.parseDouble(country[i]);
+	                row=row+1;
+	            }
+
+	        } catch (FileNotFoundException e) {
+	            e.printStackTrace();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        } finally {
+	            if (br != null) {
+	                try {
+	                    br.close();
+	                } catch (IOException e) {
+	                    e.printStackTrace();
+	                }
+	            }
+	        }
+	
+
+        int[][] r = new int[count][count];
+        int[][] g = new int[count][count];
+        int[][] b = new int[count][count];
+        
+        for(int j=0; j<count;j++) 
+        {
+        	for (int k=0;k<count;k++) {
+        		
+        		if(DoubleMatrix[j][k]>0.008 && DoubleMatrix[j][k]<=0.01) {
+        			r[j][k]=0xFF;
+        			g[j][k]=0x00;
+        			b[j][k]=0x00;
+        			}
+        		else if(DoubleMatrix[j][k]<-0.008&& DoubleMatrix[j][k]>=-0.01) {
+        			r[j][k]=0xFF;
+        			g[j][k]=0xFF;
+        			b[j][k]=0x00;
+        			}
+        		else if(DoubleMatrix[j][k]>0.01 && DoubleMatrix[j][k]<=0.025) {
+        			r[j][k]=0x00;
+        			g[j][k]=0xFF;
+        			b[j][k]=0x00;
+        		}
+        		else if(DoubleMatrix[j][k]<-0.01 && DoubleMatrix[j][k]>=-0.025) {
+        			r[j][k]=0x00;
+        			g[j][k]=0x00;
+        			b[j][k]=0xFF;
+        		}
+        		else if(DoubleMatrix[j][k]>0.025 && DoubleMatrix[j][k]<=0.05) {
+        			r[j][k]=0x00;
+        			g[j][k]=0xFF;
+        			b[j][k]=0xFF;
+        		}
+        		else if(DoubleMatrix[j][k]<-0.025 && DoubleMatrix[j][k]>-0.05) {
+        			r[j][k]=0xFF;
+        			g[j][k]=0xC7;
+        			b[j][k]=0x50;
+        		}
+        		else if(DoubleMatrix[j][k]>0 && DoubleMatrix[j][k]<=0.008) {
+        			r[j][k]=0x83;
+        			g[j][k]=0xFE;
+        			b[j][k]=0x93;
+        		}
+        		else if(DoubleMatrix[j][k]<0 && DoubleMatrix[j][k]>=-0.008) {
+        			r[j][k]=0x56;
+        			g[j][k]=0xBE;
+        			b[j][k]=0xE9;
+        		}
+        		else if(DoubleMatrix[j][k]<-0.05&& DoubleMatrix[j][k]>=-0.1) {
+        			r[j][k]=0x6C;
+        			g[j][k]=0x89;
+        			b[j][k]=0xEE;
+        			}
+        		
+        		else if(DoubleMatrix[j][k]>0.05 && DoubleMatrix[j][k]<=0.1) {
+        			r[j][k]=0x84;
+        			g[j][k]=0xFF;
+        			b[j][k]=0x00;
+        			}
+        		else if(DoubleMatrix[j][k]<-0.1) {
+        			r[j][k]=0xE8;
+        			g[j][k]=0xBA;
+        			b[j][k]=0x00;
+        			}
+        		else if(DoubleMatrix[j][k]>0.1) {
+        			r[j][k]=0xFF;
+        			g[j][k]=0x76;
+        			b[j][k]=0x5D;
+        			}
+        		else {
+        			r[j][k]=0x00;
+        			g[j][k]=0x00;
+        			b[j][k]=0x00;
+        			}
+        		
+        		/*
+        		if(DoubleMatrix[j][k]>0.008 && DoubleMatrix[j][k]<=0.01) {
+        			r[j][k]=0x48;
+        			g[j][k]=0xC8;
+        			b[j][k]=0xEF;
+        			}
+        		else if(DoubleMatrix[j][k]<-0.008&& DoubleMatrix[j][k]>=-0.01) {
+        			r[j][k]=0x6D;
+        			g[j][k]=0x00;
+        			b[j][k]=0xFF;
+        			}
+        		else if(DoubleMatrix[j][k]>0.01 && DoubleMatrix[j][k]<=0.025) {
+        			r[j][k]=0xC3;
+        			g[j][k]=0x2E;
+        			b[j][k]=0xD4;
+        		}
+        		else if(DoubleMatrix[j][k]<-0.01 && DoubleMatrix[j][k]>=-0.025) {
+        			r[j][k]=0xAC;
+        			g[j][k]=0x06;
+        			b[j][k]=0xFE;
+        		}
+        		else if(DoubleMatrix[j][k]>0.025 && DoubleMatrix[j][k]<=0.05) {
+        			r[j][k]=0x49;
+        			g[j][k]=0xE1;
+        			b[j][k]=0xC2;
+        		}
+        		else if(DoubleMatrix[j][k]<-0.025 && DoubleMatrix[j][k]>-0.05) {
+        			r[j][k]=0x68;
+        			g[j][k]=0xEA;
+        			b[j][k]=0xB4;
+        		}
+        		else if(DoubleMatrix[j][k]>0 && DoubleMatrix[j][k]<=0.008) {
+        			r[j][k]=0x15;
+        			g[j][k]=0x74;
+        			b[j][k]=0x8A;
+        		}
+        		else if(DoubleMatrix[j][k]<0 && DoubleMatrix[j][k]>=-0.008) {
+        			r[j][k]=0x4C;
+        			g[j][k]=0x5F;
+        			b[j][k]=0x72;
+        		}
+        		else if(DoubleMatrix[j][k]<-0.05&& DoubleMatrix[j][k]>=-0.1) {
+        			r[j][k]=0x17;
+        			g[j][k]=0x33;
+        			b[j][k]=0xAB;
+        			}
+        		
+        		else if(DoubleMatrix[j][k]>0.05 && DoubleMatrix[j][k]<=0.1) {
+        			r[j][k]=0x9A;
+        			g[j][k]=0x53;
+        			b[j][k]=0xD0;
+        			}
+        		else if(DoubleMatrix[j][k]<-0.1) {
+        			r[j][k]=0x1E;
+        			g[j][k]=0xD0;
+        			b[j][k]=0xAF;
+        			}
+        		else if(DoubleMatrix[j][k]>0.1) {
+        			r[j][k]=0x98;
+        			g[j][k]=0x8B;
+        			b[j][k]=0xD8;
+        			}
+        		else {
+        			r[j][k]=0x00;
+        			g[j][k]=0x00;
+        			b[j][k]=0x00;
+        			}
+        		*/
+        	}
+        	
+        	
+        }
+        
+        int width = count;
+        int height = count;
+ 
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB); 
+ 
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int rgb = r[y][x];
+                rgb = (rgb << 8) + g[y][x]; 
+                rgb = (rgb << 8) + b[y][x];
+                image.setRGB(x, y, rgb);
+            }
+        }
+ 
+  
+        
+        // Uncomment  to save the image 
+        File outputFile = new File("output.bmp");
+         ImageIO.write(image, "bmp", outputFile);
+    }
+}
---
+++ TMcor.java
@@ -0,0 +1,162 @@
+	import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.math.BigInteger;
+
+	
+public class TMcor {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		Scanner Obj = new Scanner(System.in);
+		System.out.println("How large of an art do you want to create?");
+		int input=Obj.nextInt();
+		Obj.close();
+
+		System.setOut(new PrintStream(new FileOutputStream("test.csv")));
+		for(int j=0;j<1+input;j++)
+		{		String outstring="";
+		
+			for(int k=0;k<1+input;k++)
+		{	
+			int[] loopinput= new int[4];
+			loopinput[0]=1;
+			loopinput[1]=j;
+			loopinput[2]=k;
+			loopinput[3]=input;
+			
+			int[][][] output=tree(loopinput);
+			int lastlevel=1+(int) Math.ceil(Math.log ((double) input)/Math.log(2));
+			int oneplus=0;
+			int oneminus=0;
+			int zeroplus=0;
+			int zerominus=0;
+			for(int i=0;i<Math.pow(2,lastlevel-1);i++) {
+				int sum =output[1][i][lastlevel-1]+output[2][i][lastlevel-1]+output[3][i][lastlevel-1];
+			if(sum%2==0 && output[0][i][lastlevel-1]==1) zeroplus=zeroplus+1;
+			if(sum%2==0 && output[0][i][lastlevel-1]==-1) zerominus=zerominus+1;
+			if(sum%2==1 && output[0][i][lastlevel-1]==1) oneplus=oneplus+1;
+			if(sum%2==1 && output[0][i][lastlevel-1]==-1) oneminus=oneminus+1;
+			}
+			int zerocalc=zeroplus-zerominus;
+			int onecalc=oneplus-oneminus;
+			/*if answer is a fraction
+			 * 
+			int answernum= 3* zerocalc-onecalc;
+			int answerdenum =(int)Math.pow(2,lastlevel-1)*3;
+			
+			  if(answernum==0) answerdenum=1;
+			else {int gcd=BigInteger.valueOf(answernum).gcd(BigInteger.valueOf(answerdenum)).intValue();
+			
+				answernum=answernum/gcd;
+			answerdenum=answerdenum/gcd;}
+			
+			outstring=outstring+""+ answernum+"/"+answerdenum+",";
+			*/
+			double answer=(zerocalc*1.0-onecalc*(1.0/3))/Math.pow(2,lastlevel-1);
+			outstring=outstring+""+ answer+",";
+			
+		} System.out.println(outstring);
+		}
+			
+		}
+		
+		
+		
+/* Code if we want user input	
+ * 
+ * 
+ Scanner reader = new Scanner(System.in);  // Reading from System.in
+		System.out.println("This program takes three integers as input. Please enter the first integer: ");
+		input[1] = reader.nextInt();
+		System.out.println(" Please enter the second integer: ");
+		input[2] = reader.nextInt();
+		System.out.println(" Please enter the third integer. This should be the largest one: ");
+		input[3] = reader.nextInt();
+		System.out.println("Our three integers are "+input[1]+" "+input[2]+" "+input[3]);
+		reader.close();
+	*/
+		
+		/* Code for a single triple
+		 * 
+		 * 
+		int xyz=input[1]+input[3]-input[2];
+		System.out.println ("x+z-y is equal to "+ xyz);
+int[][][] output=tree(input);
+int lastlevel=1+(int) Math.ceil(Math.log ((double) input[3])/Math.log(2));
+int oneplus=0;
+int oneminus=0;
+int zeroplus=0;
+int zerominus=0;
+
+for(int i=0;i<Math.pow(2,lastlevel-1);i++) {
+	int sum =output[1][i][lastlevel-1]+output[2][i][lastlevel-1]+output[3][i][lastlevel-1];
+if(sum%2==0 && output[0][i][lastlevel-1]==1) zeroplus=zeroplus+1;
+if(sum%2==0 && output[0][i][lastlevel-1]==-1) zerominus=zerominus+1;
+if(sum%2==1 && output[0][i][lastlevel-1]==1) oneplus=oneplus+1;
+if(sum%2==1 && output[0][i][lastlevel-1]==-1) oneminus=oneminus+1;
+}
+System.out.println("The number of (0+) terms is: " + zeroplus);
+System.out.println("The number of (0-) terms is: " + zerominus);
+System.out.println("The number of (1+) terms is: " + oneplus);
+System.out.println("The number of (1-) terms is: " + oneminus);
+
+int zerocalc=zeroplus-zerominus;
+int onecalc=oneplus-oneminus;
+int answernum= 3* zerocalc-onecalc;
+int answerdenum =(int)Math.pow(2,lastlevel-1)*3;
+if(answernum==0) answerdenum=1;
+else {int gcd=BigInteger.valueOf(answernum).gcd(BigInteger.valueOf(answerdenum)).intValue();
+answernum=answernum/gcd;
+answerdenum=answerdenum/gcd;
+}
+System.out.println("The value of the correlation is: " + answernum+"/"+answerdenum );
+
+	}
+**/	
+	
+	public static int[][] recursion (int[] start) {
+		
+		int[][] placeholder= new int[4][2];
+placeholder[1][0]=(int) Math.floor(((float) start[1])/2);		
+placeholder[2][0]=(int) Math.floor(((float) start[2])/2);
+placeholder[3][0]=(int) Math.floor(((float) start[3])/2);	
+placeholder[1][1]=(int) Math.ceil(((float) start[1])/2);		
+placeholder[2][1]=(int) Math.ceil(((float) start[2])/2);
+placeholder[3][1]=(int) Math.ceil(((float) start[3])/2);	
+		
+placeholder[0][0]=start[0];
+if(placeholder[1][0]!=placeholder[1][1]) placeholder[0][0]=placeholder[0][0]*(-1);
+if(placeholder[2][0]!=placeholder[2][1]) placeholder[0][0]=placeholder[0][0]*(-1);
+if(placeholder[3][0]!=placeholder[3][1]) placeholder[0][0]=placeholder[0][0]*(-1);
+
+placeholder[0][1]=placeholder[0][0];
+		return placeholder;
+	}
+	
+	//The output of tree is [sign,x,y,z][horizontal level][vertical level]
+	public static int[][][] tree (int[] root){
+		int levels=1+(int) Math.ceil(Math.log ((double) root[3])/Math.log(2));
+		int[][][] x=new int[4][(int) Math.pow(2,levels-1)][levels];
+		for(int i=0;i<4;i++) x[i][0][0]=root[i];
+		for(int ell=1; ell<levels;ell++) {
+		int horz=(int)Math.pow(2,ell);
+		for(int j=0;j<horz/2;j++) {
+			int[] temp1= new int[4];
+			int[][] temp2= new int[4][2];
+			for(int i=0;i<4;i++)temp1[i]=x[i][j][ell-1];
+			temp2=recursion(temp1);
+			for(int i=0;i<4;i++) {
+		x[i][2*j][ell]=temp2[i][0];
+		x[i][2*j+1][ell]=temp2[i][1];
+			}
+			}
+		}
+		
+		
+		return x;
+	}
+	}
+	
+
