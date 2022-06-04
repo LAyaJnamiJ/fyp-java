c90ac9c25919509c041bd1ef1aa9c79307a1aac3
LAyaJnamiJ <layajnamij@gmail.com>
2022-06-04 13:53:10 +0800

    Somehow got the dependency to work I guess

--- api.java
+++ api.java
@@ -1,27 +1,35 @@
-import java.io.*;
 import java.net.*;
-import java.util.Scanner;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import org.json.*;
+
+
+
 
 
 public class api {
     public static void main(String[] args) throws Exception {
             
-        String url = "http://localhost:1337/api/coefficients";
-        String charset = "java.nio.charset.StandardCharsets.UTF_8.name()";
-        String param1 = "xyz";
-        String param2 = "value";
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:1337/api/coefficients?fields[0]=value&fields[1]=x&fields[2]=y&fields[3]=z")).build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenApply(api::parse)
+                .join();
+    }
 
-        String query = String.format("param1=%s&param2=%s",
-            URLEncoder.encode(param1, charset),
-            URLEncoder.encode(param2, charset));
-        
-        URLConnection connection = new URL(url + "?" + query).openConnection();
-        connection.setRequestProperty("Accept-Charset", charset);
-        InputStream response = connection.getInputStream();
-
-        try (Scanner scanner = new Scanner(response)) {
-            String responseBody = scanner.useDelimiter("\\A").next();
-            System.out.println(responseBody);
+    public static String parse(String responseBody) {
+        JSONArray coefficients = new JSONArray(responseBody);
+        for (int i = 0; i < coefficients.length(); i++) {
+            JSONObject coefficient = coefficients.getJSONObject(i);
+            int x = coefficient.getInt("x");
+            int y = coefficient.getInt("y");
+            int z = coefficient.getInt("z");
+            int value = coefficient.getInt("value");
+            System.out.println(x + "," + y + "," + z + "," + value);
+            
         }
+        return null;
     }
 }
