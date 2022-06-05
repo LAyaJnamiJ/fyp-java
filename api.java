e407d06a6731ebadf53190e4aa801c343ba9e5c1
LAyaJnamiJ <layajnamij@gmail.com>
2022-06-06 00:22:40 +0800

    did some testing

--- api.java
+++ api.java
@@ -3,33 +3,70 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import org.json.*;
-
-
-
+import java.io.*;
 
 
 public class api {
     public static void main(String[] args) throws Exception {
-            
+        
+        Integer x = 1;
+        Integer y = 1;
+        Integer z = 1;
+
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:1337/api/coefficients?fields[0]=value&fields[1]=x&fields[2]=y&fields[3]=z")).build();
+        String path = "http://localhost:1337/api/coefficients";
+        String query = "filters[x][$eq]=%d&filters[y][$eq]=%d&filters[z][$eq]=%d";
+        String format = String.format(query, x, y, z);
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(path + "?" + format)).build();
+        System.out.println(request);   
+        
         client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body)
-                .thenApply(api::parse)
-                .join();
+            .thenApply(HttpResponse::body)
+            .thenApply(api::parse)
+            .join();
+
+        double test = 0;
+        
+            String charset = "UTF-8";
+            URLConnection connection = new URL(path).openConnection();
+            connection.setDoOutput(true); // Triggers POST.
+            connection.setRequestProperty("Accept-Charset", charset);
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=" + charset);
+            
+            try (OutputStream output = connection.getOutputStream()) {
+                output.write(format.getBytes(charset));
+            }
+            
+            InputStream response = connection.getInputStream();
     }
 
     public static String parse(String responseBody) {
-        JSONArray coefficients = new JSONArray(responseBody);
-        for (int i = 0; i < coefficients.length(); i++) {
-            JSONObject coefficient = coefficients.getJSONObject(i);
-            int x = coefficient.getInt("x");
-            int y = coefficient.getInt("y");
-            int z = coefficient.getInt("z");
-            int value = coefficient.getInt("value");
-            System.out.println(x + "," + y + "," + z + "," + value);
+        JSONObject coefficients = new JSONObject(responseBody);
+        JSONArray data = coefficients.getJSONArray("data");
+        for (int i = 0; i < data.length(); i++) {
+            JSONObject convert = data.getJSONObject(i);
+            JSONObject attribute = convert.getJSONObject("attributes");
+            Float value = attribute.getFloat("value");
+            int x = attribute.getInt("x");
+            int y = attribute.getInt("y");
+            int z = attribute.getInt("z");
+            System.out.println(x+","+y+","+z+","+value);
+            if (value != null) {
+                System.out.println(value);
+            } 
+        
             
         }
         return null;
     }
+
+    /*public static void qs(String[] args) throws Exception {
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("javascript");
+
+        // evaluate JavaScript code
+        engine.eval("print('Hello, World')");
+    }*/
+
 }
+
